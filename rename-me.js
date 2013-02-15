50f912233009e3836d6e50cc5fae1d99d3c36844
P85145 <cdwroc@gmail.com>
2013-02-14 20:56:40 -0500

    Update rename-me.js
    Updated js file.

--- rename-me.js
+++ rename-me.js
@@ -38,6 +38,10 @@ var txt="She needs more options", "Can't wear the same thing each day.";
 }
  
 // 2nd paragraph
+var txt="It is time to" + shop;
+var shop=function(purchases) + ["heels", "sundress", "party dress","casual wear"];
+var txt="I have extra money to spend";
+var money=$200
 // While Loop
 while (purchases=purchase-(purchase*.50)
 {
@@ -52,7 +56,10 @@ for (time=0; purchases > money; purchases++)
 {
 document.write(money[time] + "<br>";
 }
- 
+var txt="She has things for her trip.", "She feels good about the items";
+var emootion=function(smiles inside) + ["relief", "excitement", "content"];
+var txt="She hears her friend saying...";
+var return="Did you think you bought enough?" + groupmember;
 // Last sentence
 var txt="She wondered";
 (fitclothes.suitcase());
