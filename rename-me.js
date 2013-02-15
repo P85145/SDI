8408f010b6e2970b3f6a8cbffe900e1e2b7df1da
P85145 <cdwroc@gmail.com>
2013-02-14 22:27:02 -0500

    Update rename-me.js
    Updated script.

--- rename-me.js
+++ rename-me.js
@@ -1,46 +1,51 @@
 alert("JavaScript works!");
-// JavaScript Document
-// Cerelia Williams
+
+// Author: Cerelia Williams
 // 2/14/13
 // Project 2
 // Planning a trip to Vegas in 4 months.
  
-var txt="Our Vegas trip";
+var text="Our Vegas trip";
 var groupmember= "Stacey";
+ console.log("Our Vegas trip", "Stacey")
+	
 var myBoolean= new true ("she is already preparing");
 // Return Boolean
 {
     if (confirm("She is already preparing" + "new purchases")) {
-    return true;
-    } else {
-    return false;
+    true;
+    } else { console.log("She is preparing")
+    false;
+    console.log("She has more to buy")
     }
 }
  
 var time =4 + "months left.";
-var txt="Started shopping already";
+	console.log("She has 4 months left.")
+var text="Started shopping already";
 var purchases=["swimsuit", "sandals", "dinner gown"];
-return purchases;
+	console.log("She has already made a few purchases.")
+	purchases;
 // Return Array
 }
     showArray(the Array){
-    var qoute = "swimsuit", "sandals", "dinner gown";
-    return quote;
+    var text = "swimsuit", "sandals", "dinner gown";
+    text;
 }
  
-var txt="She needs more options", "Can't wear the same thing each day.";
+var text="She needs more options", "Can't wear the same thing each day.";
 }
     if (confirm("She needs more options", "Can't wear the same thing each day.")) {
-    return true;
+    true;
     } else {
-    return false;
+    false;
     }
 }
  
 // 2nd paragraph
-var txt="It is time to" + shop;
+var text="It is time to" + shop;
 var shop=function(purchases) + ["heels", "sundress", "party dress","casual wear"];
-var txt="I have extra money to spend";
+var text="I have extra money to spend";
 var money=$200
 // While Loop
 while (purchases=purchase-(purchase*.50)
@@ -56,10 +61,12 @@ for (time=0; purchases > money; purchases++)
 {
 document.write(money[time] + "<br>";
 }
-var txt="She has things for her trip.", "She feels good about the items";
+var text="She has things for her trip.", "She feels good about the items";
 var emootion=function(smiles inside) + ["relief", "excitement", "content"];
-var txt="She hears her friend saying...";
-var return="Did you think you bought enough?" + groupmember;
+var text="She hears her friend saying...";
+var return="Do you think you bought enough?" + groupmember;
+
 // Last sentence
-var txt="She wondered";
+var text="She wondered";
 (fitclothes.suitcase());
+ console.log("She Wondered.")
