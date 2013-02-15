23a99175f07beea7ff17f24fb8917ad3bb9a89a7
P85145 <cdwroc@gmail.com>
2013-02-14 19:56:56 -0500

    Update rename-me.js
    Added new codes

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,33 @@
 alert("JavaScript works!");
+// JavaScript Document
+// Cerelia Williams
+// 2/14/13
+// Project 2
+// Planning a trip to Vegas in 4 months.
+ 
+var txt="Our Vegas trip";
+var groupmember= "Stacey";
+var myBoolean= new true ("she is already preparing");
+var time =4 + "months left.";
+var txt="Started shopping already";
+var purchases=["swimsuit", "sandals", "dinner gown"];
+ 
+// 2nd paragraph
+// While Loop
+while (purchases-(.50)
+{
+"Keep shopping" + "<brk>"; "Let's Go!";
+}
+ 
+ 
+// 3rd paragraph
+// For Loop
+var ;
+for (time=0; purchases > money; purchases++)
+{
+document.write(money[time] + "<br>";
+}
+ 
+// Last sentence
+var txt="She wondered";
+(fitclothes.suitcase());
